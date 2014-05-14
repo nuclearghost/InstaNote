8b739a101fb06ba24d762de023096ae1bec01739
Mark Meyer <mark.meyer37@gmail.com>
2014-05-14 08:48:16 -0500

    Initial Commit

---
+++ InstaNote/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  InstaNote
+//
+//  Created by Mark Meyer on 5/14/14.
+//  Copyright (c) 2014 Mark Meyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ InstaNoteTests/InstaNoteTests.m
@@ -0,0 +1,34 @@
+//
+//  InstaNoteTests.m
+//  InstaNoteTests
+//
+//  Created by Mark Meyer on 5/14/14.
+//  Copyright (c) 2014 Mark Meyer. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface InstaNoteTests : XCTestCase
+
+@end
+
+@implementation InstaNoteTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
